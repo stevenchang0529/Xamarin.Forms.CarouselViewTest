2fb91d901b62ef0fc7206abe117d5671af853407
Steven <steven@MSI-Steven>
2018-12-30 10:03:14 +0800

    加入專案檔案。

---
+++ Xamarin.Forms.CarouselViewTest/Xamarin.Forms.CarouselViewTest/DataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin.Forms.CarouselViewTest
+{
+
+    public class DataModel
+    {
+        public List<string> Items { get; set; } = new List<string>();
+        public DataModel()
+        {
+            for (int i = 0; i < 6; i++)
+                this.Items.Add($"第{i}個");
+        }
+    }
+}
---
+++ Xamarin.Forms.CarouselViewTest/Xamarin.Forms.CarouselViewTest/MyCarouselPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms.Xaml;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+namespace Xamarin.Forms.CarouselViewTest
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MyCarouselPage : CarouselPage
+	{
+		public MyCarouselPage ()
+		{
+
+            InitializeComponent();
+		}
+	}
+}
